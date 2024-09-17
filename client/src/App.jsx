cbd19149b61e652285d6257a7f926ced1759ad6b
ADMIN <maks.volontirov@gmail.com>
2024-09-18 01:05:31 +0300

    GetPost

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,52 @@ import './App.css'
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const [title, setTile] = useState('')
+  const [status, setStatus] = useState('pending')
+  const [dueDate, setDueDate] = useState('')
+  const [file, setFile] = useState(null)
+
   useEffect(() => {
     fetch("http://localhost:3000")
-    .then((data) => data.json())
-    .then((data) => setTasks(data))
+      .then((data) => data.json())
+      .then((data) => setTasks(data))
   }, []);
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    // const task = {
+    //   title: title,
+    //   dueDate: dueDate,
+    //   status: status
+    // }
+    const task = new FormData()
+    task.append('title', title)
+    task.append('dueDate', dueDate)
+    task.append('status', status)
+    task.append('file', file)
+
+    fetch("http://localhost:3000/add", {
+      method: "POST",
+      body: task,
+    })
+      .then((data) => data.json())
+      .then((data) => setTasks((prevTasks) => [...prevTasks, data]))
+  }
+
   return (
     <div>
-      {tasks.map((task) => <div key={task.id}>{task.title}</div>)}
+      <h1>Tasks</h1>
+      <form onSubmit={handleAddTask}>
+        <input type="text" name="title" required value={title} onChange={(e) => setTile(e.target.value)}/>
+        <select name="status" value={status} onChange={(e) => setStatus(e.target.value)}>
+          <option value="pending">Pending</option>
+          <option value="completed">Completed</option>
+        </select>
+        <input type="date" name="dueDate" required value={dueDate} onChange={(e) => {setDueDate(e.target.value)}}/>
+        <input type="file" name="file" onChange={(e) => setFile(e.target.files[0])}/>
+        <button type="submit">Add Task</button>
+      </form>
+      {tasks.forEach((task) => {console.log(task)})}
+      {tasks.map((task) => <div key={task.id}>{task.title}-{task.status}-{task.dueDate}<a href={`http://localhost:3000/uploads/${task.file}`}  target="_blank">View file</a></div>)}
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -4,36 +4,37 @@
 // const path = require("path");
 import express from "express";
 import cors from "cors";
+import multer from "multer";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const storage = multer.diskStorage({
-//   destination: (_, __, cb) => {
-//     cb(null, "uploads/");
-//   },
-//   filename: (_, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-// const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
 let tasks = [];
-tasks[0] = {id: 1, title: "asda"};
-tasks[1] = {id: 2, title: "lglgo"};
 
 app.get("/", (req, res) => {
   res.status(200).json(tasks);
 });
 
-// app.post("/add", upload.single("file"), (req, res) => {
-//   const { title, status, dueDate } = req.body;
-//   const file = req.file ? req.file.filename : null;
-//   tasks.push({ title, status, dueDate, file });
-//   res.redirect("/");
-// });
+app.post("/add", upload.single("file"), (req, res) => {
+  const { title, status, dueDate } = req.body;
+  const file = req.file ? req.file.filename : null;
+  const task = { title, status, dueDate, file };
+  tasks.push(task);
+  task.id = tasks.length - 1;
+  res.status(200).json(task);
+});
 
 // app.post("/filter", (req, res) => {
 //   const { status } = req.body;
