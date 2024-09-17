30fa01823cd2eca14d3a5aa5acc3247694ae81cd
ADMIN <maks.volontirov@gmail.com>
2024-09-17 15:35:10 +0300

    lab1

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [tasks, setTasks] = useState([])
+  fetch("localhost:3000")
+  return (
+    
+  )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
+
+const app = express();
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "uploads")));
+
+let tasks = [];
+
+app.get("/", (req, res) => {
+  res.render("index", { tasks });
+});
+
+app.post("/add", upload.single("file"), (req, res) => {
+  const { title, status, dueDate } = req.body;
+  const file = req.file ? req.file.filename : null;
+  tasks.push({ title, status, dueDate, file });
+  res.redirect("/");
+});
+
+app.post("/filter", (req, res) => {
+  const { status } = req.body;
+  const filteredTasks = tasks.filter((task) => task.status === status);
+  res.render("index", { tasks: filteredTasks });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
