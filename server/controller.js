966e1aeb059dec757a2c97e86dc45cbeae82c6f0
ADMIN <maks.volontirov@gmail.com>
2024-09-19 18:39:47 +0300

    fix: refactor

--- server/controller.js
+++ server/controller.js
@@ -4,33 +4,68 @@ const Task = db.tasks;
 
 const Op = db.Sequelize.Op;
 
-export const create = (req, res, file) => {
-    const { title, dueDate, status } = req.body;
-    Task.create({ title, dueDate, status, file })
-        .then((data) => res.status(200).json(data));
-};
+class Controller {
+    create = (req, res) => {
+        const { title, dueDate, status } = req.body;
+        const file = req.file ? req.file.filename : null;
+        Task.create({ title, dueDate, status, file })
+            .then((data) => res.status(200).json(data));
+    };
 
-export const get = (req, res) => {
-    Task.findAll()
-        .then((data) => res.status(200).json(data));
-};
+    get = (req, res) => {
+        Task.findAll()
+            .then((data) => res.status(200).json(data));
+    };
 
-export const getByStatus = (req, res) => {
-    Task.findAll({ where: { status: req.params.filter } })
-        .then((data) => res.status(200).json(data));
-};
+    getByStatus = (req, res) => {
+        Task.findAll({ where: { status: req.params.filter } })
+            .then((data) => res.status(200).json(data));
+    };
 
-export const update = (req, res) => {
-    const id = req.params.id;
-    const { title, status, dueDate } = req.body;
-    Task.update({ title, status, dueDate }, { where: { id } })
-        .then((data) => res.status(201).json({ id }))
-};
+    update = (req, res) => {
+        const id = req.params.id;
+        const { title, status, dueDate } = req.body;
+        Task.update({ title, status, dueDate }, { where: { id } })
+            .then((data) => res.status(201).json({ id }))
+    };
+    
+    deleteTask = (req, res) => {
+        const id = req.params.id;
+        Task.destroy({ where: { id: id } })
+            .then(num => {
+                res.status(201).json({ id });
+            });
+    };
+}
 
-export const deleteTask = (req, res) => {
-    const id = req.params.id;
-    Task.destroy({ where: { id: id } })
-        .then(num => {
-            res.status(201).json({ id });
-        });
-};
+export default new Controller();
+// export const create = (req, res, file) => {
+//     const { title, dueDate, status } = req.body;
+//     Task.create({ title, dueDate, status, file })
+//         .then((data) => res.status(200).json(data));
+// };
+
+// export const get = (req, res) => {
+//     Task.findAll()
+//         .then((data) => res.status(200).json(data));
+// };
+
+// export const getByStatus = (req, res) => {
+//     Task.findAll({ where: { status: req.params.filter } })
+//         .then((data) => res.status(200).json(data));
+// };
+
+// export const update = (req, res) => {
+//     const id = req.params.id;
+//     const { title, status, dueDate } = req.body;
+//     Task.update({ title, status, dueDate }, { where: { id } })
+//         .then((data) => res.status(201).json({ id }))
+// };
+
+// export const deleteTask = (req, res) => {
+//     const id = req.params.id;
+//     Task.destroy({ where: { id: id } })
+//         .then(num => {
+//             res.status(201).json({ id });
+//         });
+// };
--- server/index.js
+++ server/index.js
@@ -3,11 +3,7 @@ import cors from "cors";
 import multer from "multer";
 import path from "path";
 import db from "./model/index.js";
-import { get } from "./controller.js";
-import { create } from "./controller.js";
-import { deleteTask } from "./controller.js";
-import { getByStatus } from "./controller.js";
-import { update } from "./controller.js";
+import controller from "./controller.js"
 
 const app = express();
 app.use(cors());
@@ -33,15 +29,9 @@ db.sequelize.sync()
         console.log("Failed to sync db: " + err.message);
     });
 
-app.get('/', (req, res) => {
-    get(req, res);
-});
+app.get('/', controller.get);
 
-app.post("/add", upload.single("file"), (req, res) => {
-    const file = req.file ? req.file.filename : null;
-    console.log(req.body);
-    create(req, res, file);
-});
+app.post("/add", upload.single("file"), controller.create);
 
 app.get("/uploads/:filename", (req, res) => {
     const fileName = req.params.filename;
@@ -49,17 +39,11 @@ app.get("/uploads/:filename", (req, res) => {
     res.status(200).sendFile(filePath);
 });
 
-app.delete("/delete/:id", (req, res) => {
-    deleteTask(req, res);
-});
+app.delete("/delete/:id", controller.deleteTask);
 
-app.get("/filter/:filter", (req, res) => {
-    getByStatus(req, res);
-});
+app.get("/filter/:filter", controller.getByStatus);
 
-app.put("/update/:id", (req, res) => {
-    update(req, res);
-});
+app.put("/update/:id", controller.updatec);
 
 const PORT = 3000;
 
